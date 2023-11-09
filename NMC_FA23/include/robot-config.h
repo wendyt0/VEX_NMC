9f55e8dc711a12d2cc3df35ee82c4be19a884d39
wendyt0 <100869311+wendyt0@users.noreply.github.com>
2023-11-09 16:34:34 -0500

    Add files via upload

---
+++ NMC_FA23/include/robot-config.h
@@ -0,0 +1,16 @@
+//ALL THE FOLLOWING IS TEMPLATE CODE
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern drivetrain Drivetrain;
+extern motor ArmMotor;
+extern motor ClawMotor;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
