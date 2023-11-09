ff6f08db501735715961770af11d6cfa343e424b
wendyt0 <100869311+wendyt0@users.noreply.github.com>
2023-11-09 16:33:48 -0500

    Add files via upload

---
+++ NMC_FA23/src/robot-config.cpp
@@ -0,0 +1,35 @@
+//ALL THE FOLLOWING IS TEMPLATE CODE
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor leftMotorA = motor(PORT1, ratio18_1, false);
+motor leftMotorB = motor(PORT10, ratio18_1, false);
+motor_group LeftDriveSmart = motor_group(leftMotorA, leftMotorB);
+
+motor rightMotorA = motor(PORT11, ratio18_1, true); 
+motor rightMotorB = motor(PORT20, ratio18_1, true); 
+motor_group RightDriveSmart = motor_group(rightMotorA, rightMotorB);
+
+drivetrain Drivetrain = drivetrain(LeftDriveSmart, RightDriveSmart, 319.19, 295, 40, mm, 1);
+motor ArmMotor = motor(PORT3, ratio18_1, false);
+motor ClawMotor = motor(PORT8, ratio18_1, false);
+
+// VEXcode generated functions
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
